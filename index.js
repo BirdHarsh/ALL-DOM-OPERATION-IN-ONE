060b673d2d3192ba84fa44c0e255fe3851567c65
BirdHarsh <harsh4rk@gmail.com>
2025-04-17 22:50:09 +0530

    DOM API selecting an element

---
+++ index.js
@@ -0,0 +1,35 @@
+// using getElementById
+var elem1 = document.getElementById('myBtn');
+console.log(elem1);
+
+// using getElementsByClassName
+var elem2 = document.getElementsByClassName('menu');
+console.log(elem2[0]);
+
+// using getElementsByTagName
+var elem3 = document.getElementsByTagName('div');
+console.log(elem3[0]);
+
+// using querySelector
+var elem4 = document.querySelector("#loginBtn");
+console.log(elem4);
+
+// using querySelectorAll
+var elem5 = document.querySelectorAll(".card");
+console.log(elem5[0]);
+
+// using getElementsByName
+var elem6 = document.getElementsByName("username");
+console.log(elem6[0]);
+
+// using DOM position
+var elem7 = document.body.children[0];
+console.log(elem7);
+
+// using Sibling/Parent traversal
+var elem8 = document.body.children[0].nextElementSibling;
+console.log(elem8);
+
+// using all elements
+var elem9 = document.all[5];
+console.log(elem9);
